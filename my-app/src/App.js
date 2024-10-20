d34d719cf7ab94e8fccd9c64e381ee57c1347ef5
Hugo Covarrubias <hugoenriquecovarrubias@gmail.com>
2024-10-20 16:02:41 -0500

    Creating and nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,1 +1,16 @@
-starting 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+} 

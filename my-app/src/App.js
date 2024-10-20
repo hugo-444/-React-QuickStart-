28dd1da7f8384d0d6e3eb850b0a23a87ea346eaf
Hugo C <hugoenriquecovarrubias@gmail.com>
2024-10-20 16:54:52 -0500

    displaying data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,12 +17,30 @@ function AboutPage() {
   );
 }
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+
+
 export default function MyApp() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-      <AboutPage />
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
     </div>
+    
   );
 }

76278e70dbaf7a993972821aa98b9645323f5220
Hugo C <hugoenriquecovarrubias@gmail.com>
2024-10-20 18:58:49 -0500

    responding to events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,24 @@
 import './App.css';  
 
 function MyButton() {
+  function handleClick() {
+  alert('You clicked me!');
+  }
+
   return (
-    <button>
-      I'm a button
+    <button onClick={handleClick}>
+      Click me
     </button>
   );
 }
 
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
-
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
-
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
 
 export default function MyApp() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
-
   return (
-    <ul>{listItems}</ul>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      
+    </div>
   );
 }
-
-
-
